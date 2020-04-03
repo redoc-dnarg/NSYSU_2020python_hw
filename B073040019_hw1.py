9e2b6e6b1d5d5c5e7f12241b09d240f98b14b277
redoc-dnarg <43088530+redoc-dnarg@users.noreply.github.com>
2020-04-04 00:43:01 +0800

    Add files via upload

---
+++ B073040019_hw1.py
@@ -0,0 +1,6 @@
+#B073040019
+x = "A quick brown fox jumps over the lazy dog."
+print(sorted(set(x))[-1:1-(-1):-1])
+y = "print(sorted(set(x))[-1:1-(-1):-1])"
+y += "$%&"
+print(sorted(set(y))[-1:1-(-1):-1])
