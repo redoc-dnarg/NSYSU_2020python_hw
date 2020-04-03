61d88128d156cefd02a4f256694fee4bae485e9b
redoc-dnarg <43088530+redoc-dnarg@users.noreply.github.com>
2020-04-04 00:55:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#B073040019
+z = charactersUsed = s = ""
+
+# {'n', '*', 'i', 'r', 'z', '}', '(', ')', 't', '{', 'p'}
+
+x = "hello"
+d = {}
+z=""
+exec(z); print(d)
+#
+exec(charactersUsed)
+# {'d', '*', ']', '[', '5', 'x', '='}
